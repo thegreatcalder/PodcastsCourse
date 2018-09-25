da1fe9e9dfbd96b00d859988ed4ff2258b6e43bd
Steven Calder <the.great.calder@gmail.com>
2018-09-25 08:08:50 -0600

    Set up episode table view with an array of Episode objects.

--- PodcastsCourse/Controllers/EpisodesViewController.swift
+++ PodcastsCourse/Controllers/EpisodesViewController.swift
@@ -9,9 +9,28 @@
 import UIKit
 
 class EpisodesViewController: UITableViewController {
+    var episodes = [Episode]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let episodeOne = Episode(title: "Test Episode")
+        episodes.append(episodeOne)
+        
+        let cellID = "cellID"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        tableView.dequeueReusableCell(withIdentifier: cellID)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return episodes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let episode = episodes[indexPath.row]
+        cell.textLabel?.text = episode.title
+        return cell
     }
     
     var podcast: Podcast? {
---
+++ PodcastsCourse/Models/Episode.swift
@@ -0,0 +1,13 @@
+//
+//  Episode.swift
+//  PodcastsCourse
+//
+//  Created by Steven Calder on 9/25/18.
+//  Copyright © 2018 Steven Calder. All rights reserved.
+//
+
+import Foundation
+
+struct Episode {
+    var title: String
+}
